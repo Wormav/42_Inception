7865d3502f49e4061a64b6e2d1d03adc1c475e48
Jérémy Lorette <jeremy.lorette@outlook.com>
2025-04-08 21:47:36 +0200

    a

--- srcs/wordpress/script.sh
+++ srcs/wordpress/script.sh
@@ -9,4 +9,7 @@ chmod +x wp-cli.phar
 ./wp-cli.phar config create --dbname=wordpress --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=jlorette.42.fr --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=admin@admin.com --allow-root
 
+# Création de l'utilisateur batman avec le même mot de passe que l'admin mais sans droits administrateur
+./wp-cli.phar user create batman batman@gotham.com --user_pass=$WP_PASS --role=author --allow-root
+
 exec "$@"
